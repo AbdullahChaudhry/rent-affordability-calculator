1226f6ae6bcebc742d7d229754164cc8c476fb10
Abdullah Chaudhry <19255312+AbdullahChaudhry@users.noreply.github.com>
2021-01-29 16:08:39 +0000

    remove html boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div>
+      <h1>hello</h1>
+    </div>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
