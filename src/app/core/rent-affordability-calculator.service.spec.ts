32de77d52b8c710cd9c4470debcc43388ad26763
Abdullah Chaudhry <19255312+AbdullahChaudhry@users.noreply.github.com>
2021-01-30 16:03:58 +0000

    add service tests

--- src/app/core/rent-affordability-calculator.service.spec.ts
+++ src/app/core/rent-affordability-calculator.service.spec.ts
@@ -13,4 +13,52 @@ describe('RentAffordabilityCalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('fromGrossAnnualIncome', () => {
+    it('should calculate the maximum rent per month', () => {
+      const returned = service.fromGrossAnnualIncome(18960)
+      const expected = 632
+
+      expect(returned).toEqual(expected)
+    })
+
+    it('should round to 2 decimal places', () => {
+      const returned = service.fromGrossAnnualIncome(20000)
+      const expected = 666.67
+
+      expect(returned).toEqual(expected)
+    })
+  })
+
+  describe('fromGrossMonthlyIncome', () => {
+    it('should calculate the maximum rent per month', () => {
+      const returned = service.fromGrossMonthlyIncome(1000)
+      const expected = 400
+
+      expect(returned).toEqual(expected)
+    })
+
+    it('should round to 2 decimal places', () => {
+      const returned = service.fromGrossMonthlyIncome(1001)
+      const expected = 400.40
+
+      expect(returned).toEqual(expected)
+    })
+  })
+
+  describe('fromMonthlyRent', () => {
+    it('should calculate the total gross income required', () => {
+      const returned = service.fromMonthlyRent(1000)
+      const expected = 30000
+
+      expect(returned).toEqual(expected)
+    })
+
+    it('should round to 2 decimal places', () => {
+      const returned = service.fromMonthlyRent(1001)
+      const expected = 30030
+
+      expect(returned).toEqual(expected)
+    })
+  })
 });
